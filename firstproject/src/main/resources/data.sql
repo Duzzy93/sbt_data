e089c1bd079be7ec07084a17045f22885548a1a9
yoonsuk914 <yoonsuk914@naver.com>
2025-06-24 16:54:12 +0900

    p373 스프링부트 transaction 및 index() test 완료

---
+++ firstproject/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+DELETE FROM article;
+
+INSERT INTO article (id, title, content) VALUES (1, 'aaaa', 'aaaa');
+INSERT INTO article (id, title, content) VALUES (2, 'bbbb', 'bbbb');
+INSERT INTO article (id, title, content) VALUES (3, 'cccc', 'cccc');
---
+++ firstproject/src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ArticleServiceTest {
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    void index() {
+        // 1. 예상 데이터
+        Article a = new Article(1L, "aaaa", "aaaa");
+        Article b = new Article(2L, "bbbb", "bbbb");
+        Article c = new Article(3L, "cccc", "cccc");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a,b,c));
+        // 2. 실제 데이터
+        List<Article> articles = articleService.index();
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), articles.toString());
+    }
+}
