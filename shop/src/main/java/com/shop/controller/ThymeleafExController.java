6e045a951d673372a465d31b9169b306a76684ad
yoonsuk914 <yoonsuk914@naver.com>
2025-07-04 12:53:20 +0900

    thymeleaf f 완료 및 security Config까지

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -76,6 +77,19 @@ public class ThymeleafExController {
   @GetMapping(value = "/ex05")
   public String thymeleafExample05() {
     return "thymeleafEx/thymeleafEx05";
+  }
+
+  @GetMapping(value = "/ex06")
+  public String thymeleafExample06(@RequestParam("param1") String param1,
+                                   @RequestParam("param2") String param2, Model model) {
+    model.addAttribute("param1", param1);
+    model.addAttribute("param2", param2);
+    return "thymeleafEx/thymeleafEx06";
+  }
+
+  @GetMapping(value = "/ex07")
+  public String thymeleafExample07() {
+    return "thymeleafEx/thymeleafEx07";
   }
 
 }
