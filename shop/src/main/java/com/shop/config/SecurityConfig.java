e4380d7ebb45d563ad8a5f8c67a2de89a78991f2
yoonsuk914 <yoonsuk914@naver.com>
2025-07-08 16:45:47 +0900

    security 를 이용한 로그인 및 관리자 권í한 페이지 테스트

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,10 +15,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  MemberService memberService;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    http.formLogin(Customizer.withDefaults());
+    http.formLogin(form -> form
+            .loginPage("/members/login")
+            .defaultSuccessUrl("/", true)
+            .failureUrl("/members/login/error")
+            .usernameParameter("email")
+            .passwordParameter("password")  // password 단어만 가능
+            .permitAll());
+
+    http.authorizeHttpRequests(request -> request
+            .requestMatchers("/css/**").permitAll()
+            .requestMatchers("/", "/members/**").permitAll()
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated());   // 나머지 경로는 인증을 받아야 함.
+
     http.logout(Customizer.withDefaults());
+
+    http.exceptionHandling(exception -> exception
+            .authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+
     return http.build();
   }
 
--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -3,9 +3,15 @@ package com.shop.controller;
 import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
 import com.shop.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
 @Controller
+@Slf4j
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -26,14 +33,6 @@ public class MemberController {
     model.addAttribute("memberFormDto", new MemberFormDto());
     return "member/memberForm";
   }
-
-//  @PostMapping(value = "/new")
-//  public String memberForm(MemberFormDto memberFormDto) {
-//    Member member = Member.createMember(memberFormDto, passwordEncoder);
-//    memberService.saveMember(member);
-//
-//    return "redirect:/";
-//  }
 
   @PostMapping(value = "/new")
   public String newMember(@Valid MemberFormDto memberFormDto,
@@ -52,5 +51,26 @@ public class MemberController {
     }
 
     return "redirect:/";
+  }
+
+  @GetMapping(value = "/login")
+  public String loginMember() {
+    return "/member/memberLoginForm";
+  }
+
+  @GetMapping(value = "/login/error")
+  public String loginError(Model model) {
+    model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+    return "/member/memberLoginForm";
+  }
+
+  @GetMapping("/logout")
+  public String performLogout(HttpServletRequest request, HttpServletResponse response) {
+    log.info("===============> logout");
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication != null) {
+      new SecurityContextLogoutHandler().logout(request, response, authentication);
+    }
+    return  "redirect:/";
   }
 }
